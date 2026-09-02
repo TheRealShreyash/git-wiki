c48c6cf922be66bcf16f86147a76a71bc29e1f43
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-09-02 21:44:12 +0530

    setup: inngest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from "express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.json({ message: "Healthy" });
---
+++ src/inngest/index.js
@@ -0,0 +1,5 @@
+import { inngest } from "./client";
+import { helloWorld } from "./functions/helloWorld";
+
+export { inngest };
+export const functions = [helloWorld];
