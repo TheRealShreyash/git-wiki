ba3ac79e8ccc93dad1685d1fc207787c15435a34
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-09-07 17:45:17 +0530

    chore: remove old root-level files, superseded by backend/

--- src/inngest/index.js
+++
@@ -1,7 +0,0 @@
-import { inngest } from "./client.js";
-import { helloWorld } from "./functions/helloWorld.js";
-import { indexRepo } from "./functions/indexRepo.js";
-import { askQuestionFunction } from "./functions/askQuestion.js";
-
-export { inngest };
-export const functions = [helloWorld, indexRepo, askQuestionFunction];
